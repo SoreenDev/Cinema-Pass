69bfff08f51cb16e0ba8071a6b69ea2ec1c26d07
MJ-h <soreendev@gmail.com>
2024-09-30 11:18:57 +0330

    completed migrations (adding:cascadeOnDelete)

--- database/migrations/2024_09_26_062635_create_cinemas_table.php
+++ database/migrations/2024_09_26_062635_create_cinemas_table.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Models\City;
-use App\Models\Facilities;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
