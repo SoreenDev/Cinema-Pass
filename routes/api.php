83642270eee826c742a5e24db0fe924100e6a570
MJ-h <soreendev@gmail.com>
2024-10-01 11:13:59 +0330

    bettering route structure

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\AgentController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\CinemaController;
 use App\Http\Controllers\Api\CityController;
+use App\Http\Controllers\Api\CommentController;
 use App\Http\Controllers\Api\DailyScreeningController;
 use App\Http\Controllers\Api\PerformanceController;
 use Illuminate\Http\Request;
@@ -13,9 +14,60 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('/cities', CityController::class );
-Route::apiResource('/cinemas', CinemaController::class );
-Route::apiResource('/performances', PerformanceController::class );
-Route::apiResource('/daily_screenings', DailyScreeningController::class );
-Route::apiResource('/agents', AgentController::class );
-Route::apiResource('/categories', CategoryController::class );
+
+
+Route::controller(CityController::class)->prefix('cities')->group(function () {
+    Route::get('/','index');
+    Route::post('/','store');
+    Route::get('/{city}','show');
+    Route::put('/{city}','update');
+    Route::delete('/{city}','destroy');
+});
+
+Route::controller(CinemaController::class)->prefix('cinemas')->group(function () {
+    Route::get('/','index');
+    Route::post('/','store');
+    Route::get('/{cinema}','show');
+    Route::put('/{cinema}','update');
+    Route::post('/{cinema}/comment','comment');
+    Route::delete('/{cinema}','destroy');
+});
+
+Route::controller(PerformanceController::class)->prefix('performances')->group(function () {
+    Route::get('/','index');
+    Route::post('/','store');
+    Route::get('/{performance}','show');
+    Route::put('/{performance}','update');
+    Route::post('/{performance}/comment','comment');
+    Route::delete('/{performance}','destroy');
+});
+
+Route::controller(DailyScreeningController::class)->prefix('daily_screenings')->group(function () {
+    Route::get('/','index');
+    Route::post('/','store');
+    Route::get('/{daily_screening}','show');
+    Route::put('/{daily_screening}','update');
+    Route::delete('/{daily_screening}','destroy');
+});
+
+Route::controller(AgentController::class)->prefix('agents')->group(function () {
+    Route::get('/','index');
+    Route::post('/','store');
+    Route::get('/{agent}','show');
+    Route::put('/{agent}','update');
+    Route::delete('/{agent}','destroy');
+});
+
+Route::controller(CategoryController::class)->prefix('categories')->group(function () {
+    Route::get('/','index');
+    Route::post('/','store');
+    Route::get('/{category}','show');
+    Route::put('/{category}','update');
+    Route::delete('/{category}','destroy');
+});
+
+Route::controller(CommentController::class)->prefix('comments')->group(function () {
+    Route::get('/','index');
+    Route::get('/{comment}','show');
+    Route::delete('/{comment}','destroy');
+});
