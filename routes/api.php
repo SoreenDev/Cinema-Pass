ee75e184ee15de71b4910c436d1d345224aeb590
MJ-h <soreendev@gmail.com>
2024-10-07 10:44:38 +0330

    changing Route structures

--- routes/api.php
+++ routes/api.php
@@ -22,56 +22,28 @@ Route::apiResource('agents', AgentController::class);
 
 Route::apiResource('daily_screenings', DailyScreeningController::class);
 
-Route::apiResource('scores', ScoreController::class)->except(['update','store'])->middleware('auth:sanctum');
+Route::apiResource('scores', ScoreController::class)->except(['update','store']);
 
-Route::apiResource('users', UserController::class)->middleware('auth:sanctum')->except('store');
+Route::apiResource('users', UserController::class)->except('store');
 
-Route::controller(CommentController::class)->prefix('comments')->group(function () {
-    Route::get('/','index')->middleware('auth:sanctum');
-    Route::get('/{comment}','show')->middleware('auth:sanctum');
-    Route::post('/{comment}/giving_score','givingScore')->middleware('auth:sanctum');
-    Route::delete('/{comment}','destroy')->middleware('auth:sanctum');
-});
+Route::apiResource('categories', CategoryController::class);
 
-Route::controller(CinemaController::class)->prefix('cinemas')->group(function () {
-    Route::get('/','index');
-    Route::post('/','store')->middleware('auth:sanctum');
-    Route::get('/{cinema}','show');
-    Route::put('/{cinema}','update')->middleware('auth:sanctum');
-    Route::post('/{cinema}/comment','comment')->middleware('auth:sanctum');
-    Route::post('/{cinema}/giving_score','givingScore')->middleware('auth:sanctum');
-    Route::delete('/{cinema}','destroy')->middleware('auth:sanctum');
-});
+Route::apiResource("comments", CommentController::class)->except(["store", "update"]);
+Route::post('comments/{comment}/giving_score', [CommentController::class, "givingScore"]);
 
-Route::controller(PerformanceController::class)->prefix('performances')->group(function () {
-    Route::get('/','index');
-    Route::post('/','store')->middleware('auth:sanctum');
-    Route::get('/{performance}','show');
-    Route::put('/{performance}','update')->middleware('auth:sanctum');
-    Route::post('/{performance}/comment','comment')->middleware('auth:sanctum');
-    Route::post('/{performance}/giving_score','givingScore')->middleware('auth:sanctum');
-    Route::delete('/{performance}','destroy')->middleware('auth:sanctum');
-});
+Route::apiResource('cinemas', CinemaController::class);
+Route::post('cinemas/{cinema}/comment',[CinemaController::class,'comment']);
+Route::post('cinemas/{cinema}/giving_score',[CinemaController::class,'givingScore']);
 
-Route::controller(CategoryController::class)->prefix('categories')->group(function () {
-    Route::get('/','index');
-    Route::post('/','store')->middleware('auth:sanctum');
-    Route::get('/{category}','show');
-    Route::put('/{category}','update')->middleware('auth:sanctum');
-    Route::delete('/{category}','destroy')->middleware('auth:sanctum');
-});
+Route::apiResource('performances', PerformanceController::class);
+Route::post('/{performance}/comment',[PerformanceController::class,'comment']);
+Route::post('/{performance}/giving_score',[PerformanceController::class,'givingScore']);
 
-Route::controller(UserTicketController::class)->middleware('auth:sanctum')->prefix('my_tickets')->group(function () {
-    Route::get('/','index');
-    Route::post('/','store');
-    Route::get('/{userTicket}','show');
-    Route::put('/{userTicket}','update');
-    Route::put('/{userTicket}/paid','ticketPaid');
-    Route::delete('/{userTicket}','destroy');
-});
+Route::apiResource('tickets', UserTicketController::class);
+Route::put('/{userTicket}/paid',[UserTicketController::class,'ticketPaid']);
 
 Route::controller(AuthController::class)->prefix('auth')->group(function () {
     Route::post('/login','login');
     Route::post('/register','register');
-    route::delete('/logout','logout')->middleware('auth:sanctum');
+    route::delete('/logout','logout');
 });
